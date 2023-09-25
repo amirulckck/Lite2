c865046518ba4fc2d20f0359a913cae04400191d
hokage <142867374+hokage2023@users.noreply.github.com>
2023-09-25 16:06:39 +0700

    Update restart.sh

--- menu/restart.sh
+++ menu/restart.sh
@@ -57,13 +57,10 @@ case $Restart in
                 sleep 0.5
                 echo -e "[ \033[32mok\033[0m ] Restarting websocket Service (via systemctl) "
                 sleep 0.5
-                systemctl restart sshws.service
                 systemctl restart ws-dropbear.service
                 systemctl restart ws-stunnel.service
                 sleep 0.5
                 echo -e "[ \033[32mok\033[0m ] Restarting Trojan Go Service (via systemctl) "
-                sleep 0.5
-                systemctl restart trojan-go.service 
                 sleep 0.5
                 echo -e "[ \033[32mInfo\033[0m ] ALL Service Restarted"
                 echo ""
