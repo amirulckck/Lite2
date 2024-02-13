54fa9f869a3af92f78360a333cbef4fa01d4080a
amirulckck <153341929+amirulckck@users.noreply.github.com>
2024-02-13 07:07:44 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 #########################
 
 BURIQ () {
-    curl -sS https://raw.githubusercontent.com/hokagelegend2023/ijinpremium/main/LiteIp > /root/tmp
+    curl -sS https://raw.githubusercontent.com/amirulckck/code/main/izin/ip > /root/tmp
     data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
     for user in "${data[@]}"
     do
@@ -20,9 +20,9 @@ BURIQ () {
     done
     rm -f  /root/tmp
 }
-# https://raw.githubusercontent.com/hokagelegend2023/ijinpremium/main/LiteIp 
+# https://raw.githubusercontent.com/amirulckck/code/main/izin/ip 
 MYIP=$(curl -sS ipv4.icanhazip.com)
-Name=$(curl -sS https://raw.githubusercontent.com/hokagelegend2023/ijinpremium/main/LiteIp | grep $MYIP | awk '{print $2}')
+Name=$(curl -sS https://raw.githubusercontent.com/amirulckck/code/main/izin/ip | grep $MYIP | awk '{print $2}')
 echo $Name > /usr/local/etc/.$Name.ini
 CekOne=$(cat /usr/local/etc/.$Name.ini)
 
@@ -39,7 +39,7 @@ fi
 
 PERMISSION () {
     MYIP=$(curl -sS ipv4.icanhazip.com)
-    IZIN=$(curl -sS https://raw.githubusercontent.com/hokagelegend2023/ijinpremium/main/LiteIp | awk '{print $4}' | grep $MYIP)
+    IZIN=$(curl -sS https://raw.githubusercontent.com/amirulckck/code/main/izin/ip | awk '{print $4}' | grep $MYIP)
     if [ "$MYIP" = "$IZIN" ]; then
     Bloman
     else
@@ -223,7 +223,7 @@ if [ ! -f "/etc/log-create-shadowsocks.log" ]; then
 echo "Log Shadowsocks Account " > /etc/log-create-shadowsocks.log
 fi
 history -c
-serverV=$( curl -sS https://raw.githubusercontent.com/hokagelegend2023/ijinpremium/main/lite  )
+serverV=$( curl -sS https://raw.githubusercontent.com/amirulckck/code/main/izin/ip  )
 echo $serverV > /opt/lite
 aureb=$(cat /home/re_otm)
 b=11
